76346a84aff17598aef1724cceaac049fce65012
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2020-04-27 08:33:07 +0800

    config for project

--- sample_group_UI/sampleInfo.ts
+++ sample_group_UI/sampleInfo.ts
@@ -1,34 +1,42 @@
-interface IsampleInfo {
+namespace biodeep {
 
-    /**
-     * the unique id
-    */
-    ID: string;
-    /**
-     * the display title of the sample
-    */
-    sample_name: string;
-    /**
-     * the sample group name
-    */
-    sample_info: string;
-    /**
-     * injection order in LC-MS experiment, used for batch normalization only
-    */
-    injectionOrder: number;
-    /**
-     * the LC-MS experiment batch number
-    */
-    batch: number;
-    sample_info1?: string;
-    sample_info2?: string;
-    color: string;
-    color1?: string;
-    color2?: string;
+    export interface IsampleInfo {
 
-    shape: number;
-    shape1?: number;
-    shape2?: number;
+        /**
+         * the unique id
+        */
+        ID: string;
+        /**
+         * the display title of the sample
+        */
+        sample_name: string;
+        /**
+         * the sample group name
+        */
+        sample_info: string;
+        /**
+         * injection order in LC-MS experiment, used for batch normalization only
+        */
+        injectionOrder: number;
+        /**
+         * the LC-MS experiment batch number
+        */
+        batch: number;
+        sample_info1?: string;
+        sample_info2?: string;
+        color: string;
+        color1?: string;
+        color2?: string;
 
-    delete?: string;
+        shape: number;
+        shape1?: number;
+        shape2?: number;
+
+        delete?: string;
+    }
+
+    export function as_tabular(sampleInfo: IsampleInfo[] | IEnumerator<IsampleInfo>): string {
+        return $ts.csv.toText(sampleInfo, false);
+    }
 }
+
