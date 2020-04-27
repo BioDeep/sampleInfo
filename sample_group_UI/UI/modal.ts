4c0fa5d2bc7a5a5403bcce804224dbbc1c4d9aab
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2020-04-27 13:27:29 +0800

    Update modal.ts

--- sample_group_UI/UI/modal.ts
+++ sample_group_UI/UI/modal.ts
@@ -1,15 +1,12 @@
 ﻿namespace biodeep {
 
     export function createInputModal() {
-        let modal = $ts("<div>", {
-            class: ["modal", "fade"],
-            id: "myModal",
-            tabindex: -1,
-            role: "dialog",
-            "aria-labelledby": "myModalLabel"
-        }).display(`		
-    <div class="modal-dialog" role="document">  
-        <div class="modal-content">  
+        let modalDoc = $ts("<div>", {
+            class: "modal-dialog",
+            role: "document"
+        }).display($ts("<div>", {
+            class: "modal-content"
+        }).display(`       
             <div class="modal-header">  
                 <button type="button" class="close" data-dismiss="modal" aria-label="Close">  
                     <span aria-hidden="true">×</span>  
@@ -22,9 +19,14 @@
             <div class="modal-footer">  
                 <button type="button" class="btn btn-default" data-dismiss="modal">取消</button>  
                 <button type="button" class="btn btn-primary">确定</button>  
-            </div>  
-        </div>  
-    </div> `);
+            </div>`));
+        let modal = $ts("<div>", {
+            class: ["modal", "fade"],
+            id: "myModal",
+            tabindex: -1,
+            role: "dialog",
+            "aria-labelledby": "myModalLabel"
+        }).display(modalDoc);
 
         $ts("&body").appendElement(modal);
     }
