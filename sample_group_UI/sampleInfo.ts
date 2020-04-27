d27c08daaf3162fab7fc743a11b9bfd71eed5a13
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2020-04-27 08:58:46 +0800

    Update sampleInfo.ts

--- sample_group_UI/sampleInfo.ts
+++ sample_group_UI/sampleInfo.ts
@@ -38,5 +38,33 @@ namespace biodeep {
     export function as_tabular(sampleInfo: IsampleInfo[] | IEnumerator<IsampleInfo>): string {
         return $ts.csv.toText(sampleInfo, false);
     }
+
+    export function guess_groupInfo(sampleNames: string[] | IEnumerator<string>): NamedValue<string[]>[] {
+        if (!Array.isArray(sampleNames)) {
+            sampleNames = (<IEnumerator<string>>sampleNames).ToArray();
+        }
+
+        let common: string[];
+        let column: string[];
+        let unique: string[];
+        let matrix: string[][] = $from(sampleNames).Select(s => <string[]>Strings.ToCharArray(s, false)).ToArray(false);
+        let minLen: number = $from(matrix).Min(s => s.length).length;
+
+        for (let i = 0; i < minLen; i++) {
+            column = [];
+
+            for (let name of matrix) {
+                column.push(name[i]);
+            }
+
+            unique = $from(column).Distinct().ToArray(false);
+
+            if (unique.length == 1) {
+                common.push(unique[0]);
+            } else {
+                
+            }
+        }
+    }
 }
 
