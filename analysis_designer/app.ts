29f2fc94df028068d1540032bf3e798d99bbc016
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2020-05-12 09:21:42 +0800

    designer ui

--- analysis_designer/app.ts
+++ analysis_designer/app.ts
@@ -1,1 +1,22 @@
-console.log('Hello world');
+/// <reference path="../../../build/linq.d.ts" />
+
+interface analysisDesign {
+    groups: string[];
+}
+
+interface updateDesigns { (current: analysisDesign[]): void; }
+
+/**
+ * @param id the container id
+*/
+function loadDesigner(id: string, groups: string[], currentDesigns: analysisDesign[], handler: updateDesigns = null) {
+    $ts(id).clear().appendElement(biodeep.createUI(isNullOrUndefined(handler) ? DoNothing : handler));
+
+    for (let label of groups) {
+
+    }
+
+    for (let design of currentDesigns) {
+
+    }
+}
---
+++ analysis_designer/html.ts
@@ -0,0 +1,41 @@
+namespace biodeep {
+
+    export function createUI(handler: updateDesigns): HTMLElement {
+        let all = $ts("<li>")
+            .appendElement($ts("<h3>", { class: "cen" }).display("所有样本分组"))
+            .appendElement($ts("<p>", { class: "attentionLis" }).display("按住Ctrl多选"))
+            .appendElement($ts("<div>", { id: "all_groups" }));
+        let action = $ts("<li>", { class: "groEnt" })
+            .display($ts("<i>", {
+                class: [],
+                onclick: function () {
+                    handler(getCurrentDesigns());
+                }
+            }));
+        let designerList = $ts("<li>")
+            .appendElement($ts("<h3>", { class: "cen" }).display("已选组别比对"))
+            .appendElement($ts("<p>", { class: "attentionLis" }).display("拖动组内排序"))
+            .appendElement($ts("<div>", { id: "designs" }));
+
+        return $ts("<ul>", {
+            class: "groupCount"
+        })
+            .appendElement(all)
+            .appendElement(action)
+            .appendElement(designerList);
+    }
+
+    function getCurrentDesigns(): analysisDesign[] {
+        return $ts($ts("#designs").getElementsByTagName("div"))
+            .Select(a => getDesign(a))
+            .ToArray(false);
+    }
+
+    function getDesign(label: HTMLElement): analysisDesign {
+        let labels: string[] = JSON.parse(label.getAttribute("data"));
+
+        return <analysisDesign>{
+            groups: labels
+        }
+    }
+}
